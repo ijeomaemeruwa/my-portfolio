98424c4bb32259107feaf94b2d5886446f6470d0
ijeomaemeruwa <helloijeoma@gmail.com>
2020-10-09 00:47:10 +0100

    updated home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,15 @@ import { Hero } from '../../components/Hero/Hero'
 import { Skills } from '../../components/Skills/Skills'
 //import { Projects } from '../../components/Projects/Projects'
 import { MyWork } from '../../components/MyWork/MyWork'
+import { Footer } from '../../components/Footer/Footer'
 
 export const Home = () => {
     return (
     <div className="home_page">
       <Hero /> 
       <Skills />
-      <MyWork />     
+      <MyWork /> 
+      <Footer />    
     </div>
     )
 }
